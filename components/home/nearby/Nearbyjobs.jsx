dd6b693494575230585de129b12c7671412b7b0e
keosophon@gmail.com <keosophon8888@gmail.com>
2025-10-05 10:12:37 +1300

    Implementing popular job and nearby job card

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,14 +1,42 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-import styles from './nearbyjobs.style'
+import { useRouter } from "expo-router";
+import { Text, TouchableOpacity, View } from "react-native";
+import useFetch from "../../../hook/useFetch";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
+import styles from "./nearbyjobs.style";
 
 const Nearbyjobs = () => {
-  return (
-    <View>
-      <Text>Nearbyjobs</Text>
-    </View>
-  )
-}
+  const router = useRouter();
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+  console.log(data);
 
-export default Nearbyjobs
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <Text>Loading...</Text>
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -1,35 +1,41 @@
-import { FlatList, Text, TouchableOpacity, View } from 'react-native';
-import { SIZES } from '../../../constants';
-import useFetch from '../../../hook/useFetch';
-import PopularJobCard from '../../common/cards/popular/PopularJobCard';
-import styles from './popularjobs.style';
+import { FlatList, Text, TouchableOpacity, View } from "react-native";
+import { SIZES } from "../../../constants";
+import useFetch from "../../../hook/useFetch";
+import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import styles from "./popularjobs.style";
 
 const Popularjobs = () => {
-  const { data, isLoading, error } = useFetch('search', {
-    query: 'React developer',
-    num_pages: '1'
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
   });
   console.log(data);
-  
+
   return (
-    <View style={styles.container} >
-      <View style={styles.header} >
-        <Text style={styles.headerTitle} >Popular Jobs</Text>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
         <TouchableOpacity>
-          <Text style={styles.headerBtn} >Show all</Text>
+          <Text style={styles.headerBtn}>Show all</Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.cardsContainer} >
+      <View style={styles.cardsContainer}>
         {isLoading ? (
           <Text>Loading...</Text>
         ) : error ? (
           <Text>Something went wrong</Text>
         ) : (
-          <FlatList data={[1, 2, 3, 4, 5]} renderItem={({item}) => (<PopularJobCard item={item} />)} keyExtractor={(item) => item?.job_id} contentContainerStyle={{columnGap: SIZES.medium}} horizontal/>
+          <FlatList
+            data={[1, 2, 3, 4, 5]}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
+            keyExtractor={(item) => item?.job_id}
+            contentContainerStyle={{ columnGap: SIZES.medium }}
+            horizontal
+          />
         )}
-      </View>      
+      </View>
     </View>
-  )
-}
+  );
+};
 
-export default Popularjobs
+export default Popularjobs;
